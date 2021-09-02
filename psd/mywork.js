d6afd91eaa720734338770b02b786196cd8c0efa
nouman550 <59497321+nouman550@users.noreply.github.com>
2021-09-02 23:31:19 +0500

    Add files via upload

---
+++ psd/mywork.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   
+    $('.mobile-icon').click(function(){
+        $(".menu").toggle()
+       
+          });
+      
+       //toggle menu navbar script
+       
+
+
+   
+  });
